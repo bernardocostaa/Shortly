7907ee3cbbab5110f59bd54bb21d58a741774af1
Bernardo Costa <bfvcc01@gmail.com>
2023-11-05 13:22:31 -0300

    advanced cards

--- src/advanced/Advanced.jsx
+++ src/advanced/Advanced.jsx
@@ -10,21 +10,21 @@ const Advanced = () => {
         </div>
         <div className={styles.contCard}>
             <div className={styles.card}>
-                <div>
+                <div className={styles.advancedDivImg}>
                     <img src="../../src/img/icon-brand-recognition.svg" alt="" />
                 </div>
                 <h3>Brand Recognition</h3>
                 <p>Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.</p>
             </div>
             <div className={styles.card}>
-                <div>
+                <div className={styles.advancedDivImg}>
                     <img src="../../src/img/icon-detailed-records.svg" alt="" />
                 </div>
                 <h3>Detailed Records</h3>
                 <p>Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.</p>
             </div>
             <div className={styles.card}>
-                <div>
+                <div className={styles.advancedDivImg}>
                     <img src="../../src/img/icon-fully-customizable.svg" alt="" />
                 </div>
                 <h3>Fully Customizable</h3>
